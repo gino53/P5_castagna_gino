9b29b49cfe9f1765a42160ad85e781d8edda53cf
gino53 <castagnagino5@gmail.com>
2022-08-01 19:47:57 +0200

    comm confirmation

--- Castagna_Gino_1_code_062022/front/js/confirmation.js
+++ Castagna_Gino_1_code_062022/front/js/confirmation.js
@@ -1,4 +1,4 @@
-//Récupération du numéro de commande dans l'URL
+// Récupération du numéro de commande dans l'URL
 const orderId = new URLSearchParams(document.location.search).get("orderId");
 
 document.getElementById("orderId").textContent = orderId;
